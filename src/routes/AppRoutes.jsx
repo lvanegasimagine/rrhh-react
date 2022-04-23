5f063ceb8475f4d7a3bb097d94186df39274d942
lvanegasimagine <lvanegas1429@gmail.com>
2022-04-23 17:38:22 -0600

    container para todas las rutas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -25,59 +25,57 @@ const AppRoutes = () => {
       {authIsReady && (
         <>
           <Navbar />
+          <Container maxW="container.xl">
             <Routes>
-              <Route path="login" element={user ? <DashboardScreen/> : <LoginScreen />} />
+              <Route
+                path="login"
+                element={user ? <DashboardScreen /> : <LoginScreen />}
+              />
               <Route path="signup" element={<RegistroScreen />} />
-              <Route path="/" element={user ? <DashboardScreen /> : <LoginScreen/> } />
-              <Route path="nuevo-empleado" element={user ? <NuevoEmpleadoScreen /> : <LoginScreen/>} />
+              <Route
+                path="/"
+                element={user ? <DashboardScreen /> : <LoginScreen />}
+              />
+              <Route
+                path="nuevo-empleado"
+                element={user ? <NuevoEmpleadoScreen /> : <LoginScreen />}
+              />
               <Route
                 path="editar-empleado/:id"
-                element={user ? <EditarEmpleadoScreen /> : <LoginScreen/>}
+                element={user ? <EditarEmpleadoScreen /> : <LoginScreen />}
               />
-              <Route path="listar-empleado" element={user ? <ListaEmpleadoScreen /> : <LoginScreen/>} />
+              <Route
+                path="listar-empleado"
+                element={user ? <ListaEmpleadoScreen /> : <LoginScreen />}
+              />
               <Route
                 path="nuevo-departamento"
-                element={user ? <NuevoDepartamentoScreen /> : <LoginScreen/>}
+                element={user ? <NuevoDepartamentoScreen /> : <LoginScreen />}
               />
               <Route
                 path="editar-departamento/:id"
-                element={user ? <EditarEmpleadoScreen /> : <LoginScreen/>}
+                element={user ? <EditarEmpleadoScreen /> : <LoginScreen />}
               />
               <Route
                 path="listar-departamento"
-                element={user ? <ListarDepartamentoScreen /> : <LoginScreen/>}
+                element={user ? <ListarDepartamentoScreen /> : <LoginScreen />}
               />
-              <Route path="nuevo-cargo" element={user ? <NuevoCargoScreen /> : <LoginScreen/>} />
-              <Route path="editar-cargo/:id" element={user ? <EditarCargoScreen /> : <LoginScreen/>} />
-              <Route path="listar-cargo" element={user ? <ListarCargoScreen /> : <LoginScreen/>} />
+              <Route
+                path="nuevo-cargo"
+                element={user ? <NuevoCargoScreen /> : <LoginScreen />}
+              />
+              <Route
+                path="editar-cargo/:id"
+                element={user ? <EditarCargoScreen /> : <LoginScreen />}
+              />
+              <Route
+                path="listar-cargo"
+                element={user ? <ListarCargoScreen /> : <LoginScreen />}
+              />
             </Routes>
+          </Container>
         </>
       )}
-      {/* <Navbar />
-      <Container maxW="container.xl">
-        <Routes>
-          <Route path="/" element={<DashboardScreen />} />
-          <Route path="login" element={<LoginScreen />} />
-          <Route path="nuevo-empleado" element={<NuevoEmpleadoScreen />} />
-          <Route path="editar-empleado/:id" element={<EditarEmpleadoScreen />} />
-          <Route path="listar-empleado" element={<ListaEmpleadoScreen />} />
-          <Route
-            path="nuevo-departamento"
-            element={<NuevoDepartamentoScreen />}
-          />
-          <Route
-            path="editar-departamento/:id"
-            element={<EditarDepartamentoScreen />}
-          />
-          <Route
-            path="listar-departamento"
-            element={<ListarDepartamentoScreen />}
-          />
-          <Route path="nuevo-cargo" element={<NuevoCargoScreen />} />
-          <Route path="editar-cargo/:id" element={<EditarCargoScreen />} />
-          <Route path="listar-cargo" element={<ListarCargoScreen />} />
-        </Routes>
-      </Container> */}
     </>
   );
 };
